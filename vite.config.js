6dadfb5161be63d88af6b7fd0813796533073c93
Ayesha Parveen <parveenayesha1109@gmail.com>
2026-02-02 20:33:16 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio/',   // ⬅️ THIS is the missing piece
+  base: '/portfolio/',   
   plugins: [react()],
 })
